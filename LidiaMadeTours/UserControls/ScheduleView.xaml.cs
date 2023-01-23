4ac3a981a7296ba36d92de64cab2e876b6a03f51
Lidia Popescu <lidia2popescu@gmail.com>
2023-01-24 01:05:26 +0200

    Adding styles to elements
    - The Weekly Calendar is having a grid with randomly generated colors.
    - The redundant properties for the TextBlock from ScheduleView are
    centralized in a WPF style.
    - Some code behind, generates some data to simulate busy and not tours
    stops.

--- LidiaMadeTours/UserControls/ScheduleView.xaml.cs
+++ LidiaMadeTours/UserControls/ScheduleView.xaml.cs
@@ -23,6 +23,55 @@ namespace LidiaMadeTours.UserControls
         public ScheduleView()
         {
             InitializeComponent();
+            this.Loaded += ScheduleView_loaded;
         }
+
+        private const int HourCount = 9;
+        private const int DayCount = 7;
+
+        private void ScheduleView_loaded(object sender, RoutedEventArgs e)
+        {
+            AddHoursColumn();
+            AddBusyBlock();
+        }
+
+        private void AddHoursColumn()
+        {
+            for (int i = 0; i < HourCount; i++)
+            {
+                var timeBlock = new TextBlock()
+                {
+                    Text = String.Format("{0:D2}:00 ", i + HourCount - 1)
+                };
+
+                Grid.SetColumn(timeBlock, 0);
+                Grid.SetRow(timeBlock, i + 1);
+                ScheduleGrid.Children.Add(timeBlock);
+            }
+        }
+        private void AddBusyBlock()
+        {
+            var ran = new Random();
+
+            for (int colCounter = 0; colCounter < DayCount; colCounter++)
+            {
+                for (int rowCounter = 0; rowCounter < HourCount; rowCounter++)
+                {
+                    var crowdPercent = ran.NextDouble();
+                    var box = new Rectangle()
+                    {
+                        Margin = new Thickness(1, 2, 1, 2),
+                        HorizontalAlignment = HorizontalAlignment.Stretch,
+                        Height = 60,
+                        Fill = new SolidColorBrush(Color.FromScRgb(1.0f, (float)crowdPercent, .3f, (float)(1 - crowdPercent)))
+                    };
+
+                    Grid.SetColumn(box, colCounter + 1);
+                    Grid.SetRow(box, rowCounter + 1);
+                    ScheduleGrid.Children.Add(box);
+                }
+            }
+        }
+
     }
 }
